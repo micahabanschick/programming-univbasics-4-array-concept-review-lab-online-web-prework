c0e1111fda0f676e982f26492634687b698cbefd
Micah Banschick <micha.ban@gmail.com>
2020-01-15 23:09:53 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,21 @@
 def find_element_index(array, value_to_find)
   # Add your solution here
+  if array.any?{|el| el == value_to_find}
+    array.index(value_to_find)
+  else 
+    nil 
+  end 
+  #if array.none?{|el| el == value_to_find}
+  #  nil
+  #end 
 end
 
 def find_max_value(array)
   # Add your solution here
+  array.max{|a,b|  a<=>b}
 end
 
 def find_min_value(array)
   # Add your solution here
+  array.min{|a,b|  a<=>b}
 end
